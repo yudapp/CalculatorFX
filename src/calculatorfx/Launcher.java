45792c09cf0d87ffe0f002c4126e02df52843c75
yudApp <yappadoo@hotmail.com>
2020-09-11 10:59:46 +0100

    create MVC Structure

---
+++ src/calculatorfx/Launcher.java
@@ -0,0 +1,13 @@
+package calculatorfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String [] args){ launch(args);}
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module CalculatorFX {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens calculatorfx;
+
+
+
+}
